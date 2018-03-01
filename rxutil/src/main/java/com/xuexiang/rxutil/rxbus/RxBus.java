7d2b34a2d2dc1fa688dfb06938e029625e110829
薛翔 <xuexiangjys@163.com>
2018-03-01 10:45:47 +0800

    增加RxBus工具类

---
+++ rxutil/src/main/java/com/xuexiang/rxutil/rxbus/RxBus.java
@@ -0,0 +1,116 @@
+package com.xuexiang.rxutil.rxbus;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+import rx.Observable;
+import rx.subjects.PublishSubject;
+import rx.subjects.Subject;
+
+import android.support.annotation.NonNull;
+
+/**
+ * RxBus事件通知工具
+ * @author xuexiang
+ * @date 2018/3/1 上午10:30
+ */
+public class RxBus {
+    private ConcurrentHashMap<Object, List<Subject>> maps = new ConcurrentHashMap<Object, List<Subject>>();
+    private static RxBus sInstance;
+
+    /**
+     * 获取RxBus的实例
+     * @return
+     */
+    public static RxBus get() {
+        if (sInstance == null) {
+            synchronized (RxBus.class) {
+                if (sInstance == null) {
+                    sInstance = new RxBus();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    /**
+     * 简单以对象的类名注册
+     *
+     * @param tag 类对象
+     * @return
+     */
+    public <T> Observable<T> simpleRegister(@NonNull Object tag) {
+        return register(tag.getClass().getSimpleName());
+    }
+
+    /**
+     * 注册订阅
+     *
+     * @param tag 注册标识
+     * @return
+     */
+    public <T> Observable<T> register(@NonNull Object tag) {
+        List<Subject> subjects = maps.get(tag);
+        if (subjects == null) {
+            subjects = new ArrayList<>();
+            maps.put(tag, subjects);
+        }
+        Subject<T, T> subject = PublishSubject.<T>create();
+        subjects.add(subject);
+        return subject;
+    }
+
+    /**
+     * 注销tag制定的订阅
+     *
+     * @param tag        注册标识
+     * @param observable 被观察者
+     */
+    public void unregister(@NonNull Object tag, @NonNull Observable observable) {
+        List<Subject> subjects = maps.get(tag);
+        if (subjects != null) {
+            subjects.remove(observable);
+            if (subjects.isEmpty()) {
+                maps.remove(tag);
+            }
+        }
+    }
+
+    /**
+     * 注销tag所有的订阅
+     *
+     * @param tag 注册标识
+     */
+    public void unregisterAll(@NonNull Object tag) {
+        List<Subject> subjects = maps.get(tag);
+        if (subjects != null) {
+            maps.remove(tag);
+        }
+    }
+
+    /**
+     * 发送指定tag的事件(不带内容)
+     *
+     * @param tag 注册标识
+     */
+    public void post(@NonNull Object tag) {
+        post(tag, tag);
+    }
+
+    /**
+     * 发送指定tag的事件
+     *
+     * @param tag 注册标识
+     * @param o   发生内容
+     */
+    @SuppressWarnings("unchecked")
+    public void post(@NonNull Object tag, @NonNull Object o) {
+        List<Subject> subjects = maps.get(tag);
+        if (subjects != null && !subjects.isEmpty()) {
+            for (Subject s : subjects) {
+                s.onNext(o);
+            }
+        }
+    }
+}
---
+++ rxutil/src/main/java/com/xuexiang/rxutil/rxbus/RxBusUtils.java
@@ -0,0 +1,153 @@
+package com.xuexiang.rxutil.rxbus;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.subscriptions.CompositeSubscription;
+
+/**
+ * RxBus辅助工具类
+ * @author xuexiang
+ * @date 2018/3/1 上午10:41
+ */
+public class RxBusUtils {
+    private final static String TAG = "RxBusUtils";
+
+    private static RxBusUtils sInstance;
+    private RxBus mRxBus = RxBus.get();
+    /**
+     * 管理Subscribers订阅，防止内存泄漏
+     */
+    private ConcurrentHashMap<Object, CompositeSubscription> maps = new ConcurrentHashMap<Object, CompositeSubscription>();
+
+    private RxBusUtils() {
+
+    }
+
+    public static RxBusUtils get() {
+        if (sInstance == null) {
+            synchronized (RxBusUtils.class) {
+                if (sInstance == null) {
+                    sInstance = new RxBusUtils();
+                }
+            }
+        }
+        return sInstance;
+    }
+    //===============================RxBus==================================//
+
+    /**
+     * RxBus注入监听（订阅发生在主线程）
+     *
+     * @param eventName   事件名
+     * @param action1     订阅动作
+     */
+    public <T> Observable<T> onMainThread(Object eventName, Action1<T> action1) {
+        return onMainThread(eventName, action1, new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
+                Log.e(TAG, "订阅发生错误：" + Log.getStackTraceString(throwable));
+            }
+        });
+    }
+
+    /**
+     * RxBus注入监听（订阅发生在主线程）
+     *
+     * @param eventName   事件名
+     * @param action1     订阅动作
+     * @param errorAction 错误订阅
+     */
+    public <T> Observable<T> onMainThread(Object eventName, Action1<T> action1, Action1<Throwable> errorAction) {
+        Observable<T> Observable = mRxBus.register(eventName);
+        /* 订阅管理 */
+        add(eventName, Observable.observeOn(AndroidSchedulers.mainThread()).subscribe(action1, errorAction));
+        return Observable;
+    }
+
+    /**
+     * RxBus注入监听（订阅线程不变）
+     *
+     * @param eventName   事件名
+     * @param action1     订阅动作
+     */
+    public <T> Observable<T> on(Object eventName, Action1<T> action1) {
+        return on(eventName, action1, new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
+                Log.e(TAG, "订阅发生错误：" + Log.getStackTraceString(throwable));
+            }
+        });
+    }
+
+    /**
+     * RxBus注入监听（订阅线程不变）
+     *
+     * @param eventName   事件名
+     * @param action1     订阅动作
+     * @param errorAction 错误订阅
+     */
+    public <T> Observable<T> on(Object eventName, Action1<T> action1, Action1<Throwable> errorAction) {
+        Observable<T> Observable = mRxBus.register(eventName);
+        /* 订阅管理 */
+        add(eventName, Observable.subscribe(action1, errorAction));
+        return Observable;
+    }
+
+    /**
+     * 单纯的Observables 和Subscribers管理
+     *
+     * @param eventName 事件名
+     * @param m         订阅信息
+     */
+    public void add(Object eventName, Subscription m) {
+		/* 订阅管理 */
+        CompositeSubscription subscription = maps.get(eventName);
+        if (subscription == null) {
+            subscription = new CompositeSubscription();
+            maps.put(eventName, subscription);
+        }
+        subscription.add(m);
+    }
+
+    /**
+     * 单个presenter生命周期结束，取消订阅和所有rxbus观察
+     *
+     * @param eventName 事件名
+     */
+    public void clear(@NonNull Object eventName) {
+        CompositeSubscription subscription = maps.get(eventName);
+        if (subscription != null) {
+            subscription.unsubscribe(); //取消订阅
+            maps.remove(eventName);
+        }
+        mRxBus.unregisterAll(eventName);
+
+    }
+
+    /**
+     * 发送指定tag的事件(不带内容)
+     *
+     * @param tag 注册标识
+     */
+    public void post(Object tag) {
+        mRxBus.post(tag);
+    }
+
+    /**
+     * 发送指定tag的事件
+     *
+     * @param tag     注册标识
+     * @param content 发生内容
+     */
+    public void post(Object tag, Object content) {
+        mRxBus.post(tag, content);
+    }
+
+}
