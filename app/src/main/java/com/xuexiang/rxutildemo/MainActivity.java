511008d195ff62009027ad9d8513c1afaa57a422
薛翔 <xuexiangjys@163.com>
2018-03-01 10:55:54 +0800

    增加copyright

--- app/src/main/java/com/xuexiang/rxutildemo/MainActivity.java
+++ app/src/main/java/com/xuexiang/rxutildemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.xuexiang.rxutildemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.xuexiang.rxutil.rxbus.RxBusUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
