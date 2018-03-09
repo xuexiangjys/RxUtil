e970509ef7410693bf88ecf4298dfdfab5d17f02
薛翔 <xuexiangjys@163.com>
2018-03-09 14:07:43 +0800

    提交基础订阅者BaseSubscriber

---
+++ rxutil/src/main/java/com/xuexiang/rxutil/subsciber/BaseSubscriber.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2018 xuexiangjys(xuexiangjys@163.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xuexiang.rxutil.subsciber;
+
+import android.content.Context;
+
+import com.xuexiang.rxutil.exception.RxException;
+import com.xuexiang.rxutil.exception.RxExceptionHandler;
+import com.xuexiang.rxutil.logs.RxLog;
+
+import java.lang.ref.WeakReference;
+
+import rx.Subscriber;
+
+/**
+ * 基础订阅者
+ * @author xuexiang
+ * @date 2018/3/9 上午9:00
+ */
+public abstract class BaseSubscriber<T> extends Subscriber<T> {
+
+    private WeakReference<Context> mContext;
+
+    public BaseSubscriber() {
+
+    }
+
+    /**
+     * 构造器
+     * @param context
+     */
+    public BaseSubscriber(Context context) {
+        if (context != null) {
+            mContext = new WeakReference<>(context);
+        }
+    }
+
+    @Override
+    public void onCompleted() {
+        RxLog.e("-->Subscriber is Complete");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        RxLog.e("-->Subscriber is Start");
+    }
+
+    @Override
+    public final void onError(Throwable e) {
+        RxLog.e("-->Subscriber is onError");
+        try {
+            if (e instanceof RxException) {
+                RxLog.e("--> e instanceof RxException, message:" + e.getMessage());
+                onError((RxException) e);
+            } else {
+                RxLog.e("e !instanceof RxException, message:" + e.getMessage());
+                onError(RxExceptionHandler.handleException(e));
+            }
+        } catch (Throwable throwable) {  //防止onError中执行又报错导致rx.exceptions.OnErrorFailedException: Error occurred when trying to propagate error to Observer.onError问题
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 出错
+     * @param e
+     */
+    public abstract void onError(RxException e);
+
+    /**
+     * 获取上下文
+     * @return
+     */
+    public WeakReference<Context> getContext() {
+        return mContext;
+    }
+}
